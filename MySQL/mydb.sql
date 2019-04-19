b0e5f5622d2543d47a118b10d598d315c7d182bf
Kirils_Savinihs <kirills111@gmail.com>
2019-04-19 21:05:09 +0300

    added PIT tables

--- MySQL/mydb.sql
+++ MySQL/mydb.sql
@@ -1,109 +1,122 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1
--- Время создания: Апр 19 2019 г., 16:22
--- Версия сервера: 10.1.37-MariaDB
--- Версия PHP: 7.2.12
+DROP DATABASE IF EXISTS mydb;
+CREATE DATABASE mydb;
+USE mydb;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE `tabula`
+(
+  `id`              int(6) UNSIGNED NOT NULL,
+  `name`            varchar(30)     NOT NULL,
+  `expiration_date` datetime        NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `mydb`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `tabula`
---
-
-CREATE TABLE `tabula` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `expiration_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Дамп данных таблицы `tabula`
---
-
-INSERT INTO `tabula` (`id`, `name`, `expiration_date`) VALUES
-(1, 'Text 1', '2011-12-18 13:17:17'),
-(2, 'Text2', '2015-11-16 15:12:14'),
-(3, 'Text 3', '2077-11-14 12:11:14'),
-(4, 'test', '2018-12-20 23:52:05'),
-(5, 'test2', '2018-12-20 23:52:24');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `fn` varchar(32) NOT NULL,
-  `ln` varchar(32) NOT NULL,
-  `email` varchar(32) NOT NULL,
+CREATE TABLE `users`
+(
+  `id`       int(11)     NOT NULL,
+  `fn`       varchar(32) NOT NULL,
+  `ln`       varchar(32) NOT NULL,
+  `email`    varchar(32) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `userType` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `userType` tinyint(1)  NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
---
--- Дамп данных таблицы `users`
---
-
-INSERT INTO `users` (`id`, `fn`, `ln`, `email`, `password`, `userType`) VALUES
-(1, 'Maksims', 'Gorelovs', 'maksims.gorelovs@inbox.lv', 'testadmin', 1),
-(2, 'Test', 'Test', 'test@test.com', 'testtest', 0),
-(3, 'testa', 'testb', 'taest@test.com', 'resttest', 0),
-(4, 'testte', 'testttt', 'test1@test.com', 'testtest', 0);
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `tabula`
---
 ALTER TABLE `tabula`
   ADD PRIMARY KEY (`id`);
 
---
--- Индексы таблицы `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_email_uindex` (`email`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `tabula`
---
 ALTER TABLE `tabula`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 6;
 
---
--- AUTO_INCREMENT для таблицы `users`
---
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 5;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+#PIT DB:
+
+CREATE TABLE sacensibu_grupas
+(
+  grID          int unsigned unique not null primary key auto_increment,
+  grVecumaGrupa varchar(50),
+  grKlase       varchar(10)
+);
+
+CREATE TABLE sacensibas
+(
+  sacID        int unsigned unique not null primary key auto_increment,
+  sacNosaukums varchar(50),
+  sacDatums    date,
+  sacVieta     varchar(50),
+  fk_grID      int unsigned,
+  FOREIGN KEY (fk_grID) REFERENCES sacensibu_grupas (grID)
+);
+CREATE TABLE dejotaji
+(
+  dejotID              int unsigned unique not null primary key auto_increment,
+  dejotDzimsanasDatums date,
+  dejotVardsUzvards    varchar(200),
+  dejotDzimums         varchar(1),
+  dejotVecumaGrupa     varchar(50),
+  dejotKlase           varchar(10)
+);
+
+CREATE TABLE deju_pari
+(
+  dejparID                 int unsigned unique not null primary key auto_increment,
+  dejparPartneraID         int unsigned,
+  dejparPartneresID        int unsigned,
+  dejparDibinasanasDatums  date,
+  dejparLikvidacijasDatums date
+);
+
+CREATE TABLE pieteikumi
+(
+  pietID      int unsigned unique not null primary key auto_increment,
+  pietKlase   varchar(10),
+  fk_sacID    int unsigned,
+  fk_dejparID int unsigned,
+  fk_grID     int unsigned,
+  parbaudits  boolean,
+  FOREIGN KEY (fk_sacID) REFERENCES sacensibas (sacID) ON DELETE CASCADE,
+  FOREIGN KEY (fk_dejparID) REFERENCES deju_pari (dejparID) ON DELETE CASCADE,
+  FOREIGN KEY (fk_grID) REFERENCES sacensibu_grupas (grID) ON DELETE CASCADE
+);
+
+CREATE TABLE registretie_pari
+(
+  regID     int unsigned unique not null primary key auto_increment,
+  fk_pietID int unsigned unique not null,
+  FOREIGN KEY (fk_pietID) REFERENCES pieteikumi (pietID)
+);
+
+CREATE TABLE deju_gajieni
+(
+  dejgajID        int unsigned unique not null primary key auto_increment,
+  fk_regID        int unsigned,
+  dejgajKarte     varchar(10),
+  dejgajLaiks     date,
+  dejgajNosaukums varchar(30),
+  dejgajNumurs    int,
+  FOREIGN KEY (fk_regID) REFERENCES registretie_pari (regID)
+);
+
+CREATE TABLE darbinieki
+(
+  darbID             int unsigned unique not null primary key auto_increment,
+  darbLietotajaVards varchar(50),
+  darbParole         varchar(50)
+);
+
+CREATE TABLE sacensibu_rezultati
+(
+  rezID    int unsigned unique not null primary key auto_increment,
+  vieta    int unsigned,
+  fk_regID int unsigned,
+  FOREIGN KEY (fk_regID) REFERENCES registretie_pari (regID)
+);
+

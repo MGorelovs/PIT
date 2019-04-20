5cfe63870addd68acb029976ccd516c02bc1eeb8
Kirils_Savinihs <kirills111@gmail.com>
2019-04-20 20:40:53 +0300

    добавил добавление саценсыбы

--- sacensibas.php
+++ sacensibas.php
@@ -10,25 +10,26 @@ if (session_status() == PHP_SESSION_NONE) {
 <head>
     <meta charset="UTF-8">
     <title>Maksims Gorelovs, 161RDB251</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+    <!--[if lte IE 8]>
+    <script src="js/html5shiv.js"></script><![endif]-->
     <script src="js/jquery.min.js"></script>
     <script src="js/skel.min.js"></script>
     <script src="js/skel-layers.min.js"></script>
     <script src="js/init.js"></script>
     <noscript><
-        <link rel="stylesheet" href="css/skel.css" />
-        <link rel="stylesheet" href="css/style.css" />
-        <link rel="stylesheet" href="css/style-xlarge.css" />
+        <link rel="stylesheet" href="css/skel.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
+        <link rel="stylesheet" href="css/style-xlarge.css"/>
     </noscript>
 </head>
 
 <body>
 
 <?php
-include ("header.php");
+include("header.php");
 ?>
 
 <?php
@@ -61,12 +62,11 @@ include("db.php");
                     <tbody>
                     <?php
                     $db->query("SET NAMES utf8mb4");
-                    $result = $db->query("SELECT * FROM sacensibas");
-                    echo "</br>".$db->error;
+                    $result = $db->query("SELECT * FROM sacensibas ORDER BY sacDatums DESC");
+                    echo "</br>" . $db->error;
 
 
-
-                    while($row = mysqli_fetch_assoc($result)) {
+                    while ($row = mysqli_fetch_assoc($result)) {
                         if ($_SESSION['authorized'] == "Yes") {
 
                             echo "<tr>";
@@ -90,29 +90,47 @@ include("db.php");
             </div>
 
         </section>
-    </div>
-</section>
-<?php if($_SESSION['authorized'] == "Yes"){  ?>
-<section id="three" class="wrapper style3 special">
-    <div class="container 50%">
-        <form action="add.php" method="post">
-            <div class="row uniform">
-                <div class="12u$">
-                    <textarea name="name" id="message" placeholder="Name" rows="1" maxlength="32"></textarea>
-                </div>
-                <div class="12u$">
-                    <ul class="actions">
-                        <li><input value="Pievienot" class="special big" type="submit"></li>
-                    </ul>
-                </div>
-            </div>
-        </form>
-    </div>
-</section>
-<?php } ?>
+        <?php if ($_SESSION['authorized'] == "Yes") { ?>
+        <section id="three" class="wrapper style3 special">
+            <div class="table-wrapper">
+                <form action="add.php" method="post">
+                    <div>
+                        <div>
+                            <div>
+                                <table>
+                                    <tr>
+                                        <td><input id="sacNosaukums" name="sacNosaukums" type="text"
+                                                   placeholder="Sacensibu nosaukums"></td>
+                                        <td><input id="sacDatums" name="sacDatums" type="date"
+                                                   placeholder="Sacensibu nosaukums"></td>
+                                        <td><input id="sacVieta" name="sacVieta" type="text"
+                                                   placeholder="Sacensibu vieta">
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
 
-<?php
-include ("footer.php");
-?>
+                        </div>
+                        <div class="12u$">
+                            <ul class="actions">
+                                <li><input id="submit" value="Pievienot" class="special big" type="submit"></li>
+                                <script>
+                                    $('#submit').click(function () {
+                                        if ($('#sacNosaukums').val().length == 0 || $('#sacDatums').val().length == 0 || $('#sacVieta').val().length == 0) {
+                                            alert("Ne visi lauki ir aizpilditi!");
+                                        }
+                                    })
+                                </script>
+                            </ul>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </div>
+    <?php } ?>
+    <?php
+    include("footer.php");
+    ?>
 </body>
 </html>

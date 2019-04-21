b70bde67d8e234470761cef9b46cc620b4ef9750
Kirils_Savinihs <kirills111@gmail.com>
2019-04-22 01:00:35 +0300

    тест данные для базы

---
+++ MySQL/testData.sql
@@ -0,0 +1,8 @@
+insert into sacensibas values(null,'Dick sucking festival',curdate(),'Sex dungeon');
+insert into sacensibas values(null,'Fire festival',curdate(),'Volcano');
+
+insert into dejotaji values(null,curdate(),'Kirils Savinihs','M','Teenager','Master');
+insert into dejotaji values(null,curdate(),'Some bitch','F','Teenager','Novice');
+
+insert into deju_pari values (null,1,2,curdate(),'2020-01-01');
+

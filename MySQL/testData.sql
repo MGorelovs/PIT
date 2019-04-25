eb9098dd57da254a200f84d2a1d81e68b76f90c8
MGorelov <maksims.gorelovs@inbox.lv>
2019-04-25 23:03:09 +0300

    Task 3 - Test example update

--- MySQL/testData.sql
+++ MySQL/testData.sql
@@ -1,10 +1,13 @@
 insert into sacensibas values(null,'Dick sucking festival',curdate(),'Sex dungeon');
 insert into sacensibas values(null,'Fire festival',curdate(),'Volcano');
 
-insert into dejotaji values(null,curdate(),'Kirils Savinihs','M','Teenager','Master');
-insert into dejotaji values(null,curdate(),'Some bitch','F','Teenager','Novice');
+insert into dejotaji values(null,curdate(),'Kirils Savinihs','M','Pieaugušie','B');
+insert into dejotaji values(null,curdate(),'Some bitch','F','Jaunieši','B');
+insert into dejotaji values(null,curdate(),'Maksims Gorelovs','M','Pieaugušie','A');
+insert into dejotaji values(null,curdate(),'Kim Kardashian','F','Pieaugušie','C');
 
-insert into deju_pari values (null,1,2,curdate(),'2020-01-01');
+insert into deju_pari values (null,1,2,curdate(),null);
+insert into deju_pari values (null,3,4,curdate(),null);
 
 insert into sacensibu_grupas values (null,'Adult1','Class1',1);
 insert into sacensibu_grupas values (null,'Adult3','Class3',1);

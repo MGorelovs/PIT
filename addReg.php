d8100faf7d242e50512a0e5d9f923dc7c4815468
MGorelov <maksims.gorelovs@inbox.lv>
2019-04-25 23:03:54 +0300

    NOT READY - registation updates; TODO attached in top of file

--- addReg.php
+++ addReg.php
@@ -30,16 +30,12 @@ if (isset($_POST['getPairs']))
         $_SESSION['form_dejparID']=$row['dejparID'];
         $_SESSION['form_dejparPartneraID'] = $row['dejparPartneraID'];
         $_SESSION['form_dejparPartneresID'] = $row['dejparPartneresID'];
-        $_SESSION['form_dejparDibinasanasDatums'] = $row['dejparDibinasanasDatums'];
-        $_SESSION['form_dejparLikvidacijasDatums'] = $row['dejparLikvidacijasDatums'];
     }
 
     $query = $db ->query("SELECT * FROM dejotaji where dejotID = ".$_SESSION['form_dejparPartneraID']);
     while($row = mysqli_fetch_array($query))
     {
         $_SESSION['form_dejparPartneraVardsUzvards'] = $row['dejotVardsUzvards'];
-        $_SESSION['form_dejparPartneraDzimsanasDatums'] = $row['dejotDzimsanasDatums'];
-        $_SESSION['form_dejparPartneraDzimums'] = $row['dejotDzimums'];
         $_SESSION['form_dejparPartneraVecumaGrupa'] = $row['dejotVecumaGrupa'];
         $_SESSION['form_dejparPartneraKlase'] = $row['dejotKlase'];
     }
@@ -49,19 +45,25 @@ if (isset($_POST['getPairs']))
     while($row = mysqli_fetch_array($query))
     {
         $_SESSION['form_dejparPartneresVardsUzvards'] = $row['dejotVardsUzvards'];
-        $_SESSION['form_dejparPartneresDzimsanasDatums'] = $row['dejotDzimsanasDatums'];
-        $_SESSION['form_dejparPartneresDzimums'] = $row['dejotDzimums'];
         $_SESSION['form_dejparPartneresVecumaGrupa'] = $row['dejotVecumaGrupa'];
-        $_SESSION['form_dejparPartneresKlase'] = $row['dejotKlase'];
     }
     
 
 }
-else if (isset($_POST['reg']))
-{
-    $db ->query("insert into pieteikumi values (null,'test',".$_SESSION['form_sacID'].",".$_SESSION['form_dejparID'].",".$_SESSION['form_grID_array'][0].",false)");
-//    echo "insert into pieteikumi values (null,'test',".$_SESSION['form_sacID'].",".$_SESSION['form_dejparID'].",".$_SESSION['form_grID_array'][0].",'false')";
-    echo $db->error;
+else if (isset($_POST['reg'])) {
+    if (!isset($_SESSION['form_sacID'])) {
+        ?><script>alert("Nav izvēlētas sacensības!");</script><?php
+    } else if (!isset($_SESSION['form_dejotID'])) {
+        ?><script>alert("Nav izvēlēts dejotājs!");</script><?php
+    } else if (!isset($_SESSION['form_grID_array'])) {
+        ?><script>alert("Nav izvēlēta neviena grupa!");</script><?php
+    } else {
+        for ($i = 0; $i < sizeof($_SESSION['form_grID_array']); $i++) {
+            $db->query("INSERT INTO registretie_pari VALUES (null," . $_SESSION['form_sacID'] . "," . $_SESSION['form_dejparID'] . "," . $_SESSION['form_grID_array'][$i] . ")");
+//       echo "insert into pieteikumi values (null,'test',".$_SESSION['form_sacID'].",".$_SESSION['form_dejparID'].",".$_SESSION['form_grID_array'][0].",'false')";
+            echo $db->error;
+        }
+    }
 }
 header("Location: registracija.php");
 

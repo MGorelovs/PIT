0f4beee9ce205bcfe1da34b507971924b93b884b
Kirils_Savinihs <kirills111@gmail.com>
2019-05-13 22:10:32 +0300

    новые тест данные

--- MySQL/testData.sql
+++ MySQL/testData.sql
@@ -1,14 +1,61 @@
-insert into sacensibas values(null,'Dick sucking festival',curdate(),'Sex dungeon');
-insert into sacensibas values(null,'Fire festival',curdate(),'Volcano');
+insert into sacensibas
+values (null, 'Dick sucking festival', curdate() + INTERVAL 1 WEEK, 'Sex dungeon');
+insert into sacensibas
+values (null, 'Fire festival', curdate() + INTERVAL 1 WEEK, 'Volcano');
+insert into sacensibas
+values (null, 'Too late bitch', curdate() - INTERVAL 1 DAY, 'Volcano');
 
-insert into dejotaji values(null,curdate(),'Kirils Savinihs','M','Pieaugušie','B');
-insert into dejotaji values(null,curdate(),'Some bitch','F','Jaunieši','B');
-insert into dejotaji values(null,curdate(),'Maksims Gorelovs','M','Pieaugušie','A');
-insert into dejotaji values(null,curdate(),'Kim Kardashian','F','Pieaugušie','C');
+insert into dejotaji
+values (null, curdate() - INTERVAL 20 YEAR, 'Kirils Savinihs', 'M', 'Pieaugušie', 'B');
+insert into dejotaji
+values (null, curdate() - INTERVAL 20 YEAR, 'Some bitch', 'F', 'Jaunieši', 'B');
+insert into dejotaji
+values (null, curdate() - INTERVAL 20 YEAR, 'Maksims Gorelovs', 'M', 'Pieaugušie', 'A');
+insert into dejotaji
+values (null, curdate() - INTERVAL 20 YEAR, 'Kim Kardashian', 'F', 'Pieaugušie', 'C');
 
 insert into deju_pari values (null,1,2,curdate(),null);
 insert into deju_pari values (null,3,4,curdate(),null);
 
-insert into sacensibu_grupas values (null,'Adult1','Class1',1);
-insert into sacensibu_grupas values (null,'Adult3','Class3',1);
-insert into sacensibu_grupas values (null,'Adult2','Class2',2);
+# insert into sacensibu_grupas values (null,'Adult1','Class1',1);
+# insert into sacensibu_grupas values (null,'Adult3','Class3',1);
+# insert into sacensibu_grupas values (null,'Adult2','Class2',2);
+
+insert into sacensibu_grupas
+values (null, 'Child 1', 'Class1', 1);
+insert into sacensibu_grupas
+values (null, 'Child 2', 'Class2', 1);
+insert into sacensibu_grupas
+values (null, 'Junior 1', 'Class3', 1);
+insert into sacensibu_grupas
+values (null, 'Junior 2', 'Class4', 1);
+insert into sacensibu_grupas
+values (null, 'Teen', 'Class5', 1);
+insert into sacensibu_grupas
+values (null, 'Adult', 'Class6', 1);
+
+insert into sacensibu_grupas
+values (null, 'Child 1', 'Class2', 2);
+insert into sacensibu_grupas
+values (null, 'Child 2', 'Class2', 2);
+insert into sacensibu_grupas
+values (null, 'Junior 1', 'Class3', 2);
+insert into sacensibu_grupas
+values (null, 'Junior 2', 'Class4', 2);
+insert into sacensibu_grupas
+values (null, 'Teen', 'Class5', 2);
+insert into sacensibu_grupas
+values (null, 'Adult', 'Class6', 2);
+
+insert into sacensibu_grupas
+values (null, 'Child 1', 'Class3', 3);
+insert into sacensibu_grupas
+values (null, 'Child 2', 'Class2', 3);
+insert into sacensibu_grupas
+values (null, 'Junior 1', 'Class3', 3);
+insert into sacensibu_grupas
+values (null, 'Junior 2', 'Class4', 3);
+insert into sacensibu_grupas
+values (null, 'Teen', 'Class5', 3);
+insert into sacensibu_grupas
+values (null, 'Adult', 'Class6', 3);
